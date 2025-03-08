9824780c71d80cdd27994bea868af40dda6fc303
unknown <haddadihanie@gmail.com>
2025-03-08 13:26:42 +0330

    add peroperossesing part to code1.py

--- code1.py
+++ code1.py
@@ -1,1 +1,20 @@
-print"hello, this is the first attemp"
+pip install mlxtend 
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+df=pd.read_excel('/content/drive/MyDrive/step3/dataset.xlsx', engine='openpyxl')
+df.isnull().sum()
+df.info()
+df=df.drop(['pkSeqID','flgs','proto','state','subcategory','attack','stime','ltime','saddr','daddr',],axis=1)
+df.shape
+df=df[(df.sport != "0x0303" )]
+df=df[(df.sport != "0x000d" )]
+df=df[(df.sport != "0x0008" )]
+df=df[(df.sport != "0x0011" )]
+df['sport']=pd.to_numeric(df['sport'],downcast='float')
+df['dport']=pd.to_numeric(df['dport'],downcast='float')
+df= df.sample(frac = 1)
+X=X.astype({'pkts': 'float','bytes': 'float', 'seq': 'float','spkts': 'float','dpkts': 'float','sbytes': 'float','dbytes': 'float'})
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+y_org=le.fit_transform(y)
